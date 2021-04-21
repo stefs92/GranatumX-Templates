ae299267e1b01affe4fedb67e16a875c2c973e6f
stefs92 <stefs@DESKTOP-TLQFM8B.localdomain>
2021-04-20 21:23:43 -0400

    Fixing EOF on test.sh

--- R_template/test/test.sh
+++ R_template/test/test.sh
@@ -1,11 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
-#echo "Check that the granatum path is being set correctly:"
-#echo $GRANATUM_SWD
-#echo ""
-#res = `pwd`
-#echo "$res"
-#echo ""
-#echo "Running..."
 Rscript ./main.R
-
