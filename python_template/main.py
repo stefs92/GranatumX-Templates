97d79c1826cc374cba5d62784cc8e35e4205445e
stefs92 <stefs92@DESKTOP-TLQFM8B.localdomain>
2021-04-27 17:25:57 -0400

    Some additional annotation for Python scripts

--- python_template/main.py
+++ python_template/main.py
@@ -3,13 +3,36 @@ import numpy as np
 
 # Demonstration of a Gbox that transposes a Gene Expression Matrix
 def main():
+    # initialize the object used for importing/exporting data
     gn = granatum_sdk.Granatum()
+
+    # get imports
+    # imports can be accessed using keywords specified 
+    # in the "injectInto" field of the arguments section
+    # of the package.yaml file - in our case, "exampleImport"
     assay = gn.get_import("exampleImport")
+
+    # our gn.get_import() returned an object of the "assay" class
+    # this is a dictionary with 3 keywords: "matrix" contains the
+    # gene expression values, "sampleIds" contain the cell IDs
+    # and "geneIds" contain the gene IDs
+
+    # get arguments
+    # arguments can be accessed using keywords specified 
+    # in the "injectInto" field of the arguments section
+    # of the package.yaml file - in our case, "exampleArgument"
     example_arg = gn.get_arg("exampleArgument")
+
+    # INSERT THE BODY OF YOUR SCRIPT HERE
+    # for example, you may transpose the gene expression data as
     assay["matrix"] = np.array(assay.get("matrix")).T
 
+    # export results
+    # here, we use keywords specified in the "extractFrom"
+    # field in the exports section of the package.yaml file - in 
+    # our case, "exampleExport"
     gn.export_statically(assay, "exampleExport")    
-    gn.add_result("Matrix successfully transposed", data_type="markdown")
+    gn.add_result("Example of a concluding message", data_type="markdown")
     gn.commit()
 
 if __name__ == "__main__":
